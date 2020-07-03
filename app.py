cec5511536e4905a5c156cb556e9758f5fa7c8dd
mdb571 <r.mohammedbilal@gmail.com>
2020-07-03 17:03:09 +0530

    added /assignment

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ payload={
 url='https://tkmce.linways.com/student/'
 home_url=url+ 'student.php?menu=home'
 attendence_url='https://tkmce.linways.com/student/attendance/ajax/ajax_subjectwise_attendance.php?action=GET_REPORT'
+assignment_url='https://tkmce.linways.com/student/asimark/ajax_assignment_mark.php'
 
 studentdata={}
 
@@ -54,10 +55,33 @@ def attendance():
             attendict[j]['Total Hours']=s[3].string
             attendict[j]['Attendence Percentage']=s[4].string
 
-
-
     return jsonify({'subject':attendict})
 
+@app.route('/assignment',methods=['GET'])
+def assignment():
+    assignmentdict={}
+    with requests.Session() as session:
+        post = session.post(url, data=payload)
+        r = session.post(assignment_url,data={"semID":4})
+        soup=bs4.BeautifulSoup(r.text,'lxml')
+        table=soup.find_all('table',{ "class" : "table table-striped" })
+        table.pop(0)
+        # row=[i.text.replace("\n",' ') for i in head]
+        for elem in table:
+            assignmentdict[elem.thead.text.replace('\n','')]={}
+            assignmentdict[elem.thead.text.replace('\n','')]['Subject']={}
+            row=elem.find_all('tr')
+            for tr in row:
+                cols = tr.find_all('td')
+                if cols==[]:
+                     continue
+                if cols[0].text=="Subjects":
+                    continue
+                assignmentdict[elem.thead.text.replace('\n','')]['Subject'][cols[0].text]={}
+                assignmentdict[elem.thead.text.replace('\n','')]['Subject'][cols[0].text]['Marks']=cols[1].text
+                assignmentdict[elem.thead.text.replace('\n','')]['Subject'][cols[0].text]['Max Marks']=cols[2].text
+
+    return jsonify({'assignmentMarks':assignmentdict})        
 
 @app.errorhandler(404)
 def not_found(error):
